10b768fd184c605e8e2be1f59797cf255ebe8f17
LaunchEric <wangmacanghao@yahoo.com>
2018-03-17 22:12:29 -0400

    fix codeclimate

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,7 +1,7 @@
 require 'factory_bot'
 FactoryBot.define do
   factory :user do
-    sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
   end
